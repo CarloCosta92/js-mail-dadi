963c9b85a905f8dc0d54098db947a7be30b36584
Carlo Costa <carlo.costa92@gmail.com>
2025-02-27 15:02:00 +0100

    feat: solved exercise

--- mail/script.js
+++ mail/script.js
@@ -10,11 +10,41 @@ Non è necessario provvedere alla validazione delle email
 
 // creazione lista invitati
 
+let invitati = [ "carlo@gmail.com" ,"stefano@gmail.com", "sabrina@gmail.com", "jacopo@libero.it", "alessandro@libero.it","ottavia@libero.it", "boolean@css.it", "zuccherosintattico@angela.it" ];
+
 // inserimento mail persona che vuole partecipare
+
+let mailUtente = prompt("Dimmi la tua mail cosi che possiamo verificare se se stato invitato");
 
 // inserisco variabile booleana ciclo for per verificare se 
 
+let mailValida = false;
+
 //ciclo for per verificare se la persona può partecipare
+
+for (let i=0 ; i < invitati.length; i++){
+
+    if(mailUtente === invitati[i]){
+
+        console.log("Sei stato invitato,puoi entrare!!! Divertiti");
+
+        alert("Sei stato invitato,puoi entrare!!! Divertiti");
+
+        mailValida = true;
+        break;
+
+    } 
+}
+
+if (!mailValida){
+    console.log("Non sei stato invitato,non provare più ad imbucarti!");
+
+    alert("Non sei stato invitato,non provare più ad imbucarti!");
+
+}
+        
+    
+
 
 
 
