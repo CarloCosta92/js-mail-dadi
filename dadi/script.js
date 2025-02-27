650bcdcc13ee95f9ef2d6b97de87603950b7d7d9
Carlo Costa <carlo.costa92@gmail.com>
2025-02-27 15:36:16 +0100

    feat: solved dado exercise

--- dadi/script.js
+++ dadi/script.js
@@ -8,7 +8,32 @@ Invece di generare il numero random per l'utente, chiedilo con un prompt!
 
 // lista numero facce del dado
 
+let facceDado= [1,2,3,4,5,6];
+console.log(facceDado)
+
 // generazione casuale per il player e per il pc
 
+//   Math.floor(Math.random() * max) //
+
+let dadoPlayer = facceDado[Math.floor(Math.random() * facceDado.length)]; 
+console.log(dadoPlayer);
+
+let dadoComputer = facceDado[Math.floor(Math.random() * facceDado.length)]; 
+console.log(dadoComputer);
+
+
 // condizione if per verificare qual'è quello più alto
+
+if (dadoPlayer > dadoComputer) {
+
+    console.log("Hai vinto");
+
+} else if (dadoComputer > dadoPlayer) {
+
+    console.log("Hai perso, prova di nuovo");
+
+} else {
+
+    console.log("Avete lanciato lo stesso numero,pari e patta");
+}
 
